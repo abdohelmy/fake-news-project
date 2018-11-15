4469104f83534749b4d5359af9247a865ef63b14
Tarek Samir <tsamirfci@gmail.com>
2018-11-15 14:22:40 +0200

    first step
    Uplaod dataset (.csv)
    make first pre-processing on it (.py)

---
+++ untitled0.py
@@ -0,0 +1,34 @@
+import pandas as pd 
+import numpy as np 
+import re 
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer 
+nltk.download("stopwords')
+
+df = pd.read_csv('fake_or_real_news.csv')
+
+
+"""
+title =re.sub('[^a-zA-Z]' ,' ', df['title'][0])
+title = title.lower()
+title = title.split()
+ps = PorterStemmer ()
+title = [ps.stem(word) for word in title if not word in set (stopwords.words('english'))]
+title = ' '.join(title)"""
+
+ps = PorterStemmer ()
+
+def clean_text (text):
+    title =re.sub('[^a-zA-Z]' ,' ', text)
+    title = title.lower()
+    title = title.split()
+    title = [ps.stem(word) for word in title if not word in set (stopwords.words('english'))]
+    title = ' '.join(title)
+    return title
+
+df['new_column'] = df['title'].apply(clean_text)   
+
+         
+        
+            
+    
